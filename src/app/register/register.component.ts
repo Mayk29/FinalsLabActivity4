fe4b60d47fa7b5aac8e126326a5a163b0b54a162
Mayk29 <144456367+Mayk29@users.noreply.github.com>
2025-02-26 11:54:13 +0800

    Update register.component.ts

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,33 +1,47 @@
 import { Component } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
-import { FormGroup, FormControl, Validators, FormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import {MatButtonModule} from '@angular/material/button';
-import {MatInputModule} from '@angular/material/input';
-import {MatCheckboxModule} from '@angular/material/checkbox';
-import {MatFormFieldModule } from '@angular/material/form-field';
-import {MatSliderModule} from '@angular/material/slider';
-import {MatRadioModule } from '@angular/material/radio';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatNativeDateModule } from '@angular/material/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [CommonModule,MatButtonModule,MatCheckboxModule,MatFormFieldModule,MatInputModule,MatSliderModule,MatRadioModule,MatDatepickerModule,MatNativeDateModule,ReactiveFormsModule,FormsModule],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSliderModule,
+    MatRadioModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatSelectModule,
+    MatTooltipModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
   userName: string = '';
   email: string = '';
   password: string = '';
-  gender: string='';
+  gender: string = '';
   birthDate!: Date;
-  address: string='';
+  address: string = '';
   angularSkillLevel: number = 5;
   submitted = false;
-  minSkillLevel =1;
+  minSkillLevel = 1;
   maxSkillLevel = 10;
 
   formdata: FormGroup = new FormGroup({
@@ -37,10 +51,12 @@ export class RegisterComponent {
     gender: new FormControl('', [Validators.required]),
     birthDate: new FormControl(null, [Validators.required]),
     address: new FormControl(''),
-    angularSkillLevel: new FormControl(5)
+    angularSkillLevel: new FormControl(5),
+    locality: new FormControl('', [Validators.required]),
+    terms: new FormControl(false, [Validators.requiredTrue])
   });
 
-  onClickSubmit(data:{
+  onClickSubmit(data: {
     userName: string;
     email: string;
     password: string;
@@ -48,8 +64,9 @@ export class RegisterComponent {
     birthDate: Date;
     address: string;
     angularSkillLevel: number;
-  })
-  {
+    locality: string;
+    terms: boolean;
+  }) {
     this.submitted = true;
     this.userName = data.userName;
     this.email = data.email;
@@ -61,8 +78,8 @@ export class RegisterComponent {
 
     if (this.formdata.valid) {
       console.log(this.formdata.value);
-  } else{
-    console.log('Form is not valid');
+    } else {
+      console.log('Form is not valid');
+    }
   }
-}
 }
